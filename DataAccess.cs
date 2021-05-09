96dc1d09611bd213cd26fad059f1c77ca6d31904
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2021-05-10 11:48:58 +1200

    Demo DataAccess Class

---
+++ DataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAT602_tester
+{
+    class DataAccess
+    {
+        private static string connectionString
+        {
+            get { return "Server=localhost;Port=3306;Database=sapodb;Uid=sapo;password=53211;"; }
+            
+        }
+
+        private static MySqlConnection _mySqlConnection = null;
+        public static MySqlConnection mySqlConnection
+        {
+            get {
+                if (_mySqlConnection == null)
+                {
+                    _mySqlConnection = new MySqlConnection(connectionString);
+                }
+
+                return _mySqlConnection;
+                
+            }  
+        }
+        public string AddUserName(string pUserName)
+        {
+            //MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+            List<MySqlParameter> p = new List<MySqlParameter>();
+            var aP = new MySqlParameter("@UserName", MySqlDbType.VarChar, 50);
+            aP.Value = pUserName;
+            p.Add(aP);
+
+
+            var aDataSet = MySqlHelper.ExecuteDataset(DataAccess.mySqlConnection, "AddUserName(@UserName)", p.ToArray());
+
+            // expecting one table with one row
+            return (aDataSet.Tables[0].Rows[0])["MESSAGE"].ToString();
+        }
+    }
+}
