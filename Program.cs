9cdfd87a213528e71b2d83adee72a1a8c41c22a0
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2021-05-10 12:07:34 +1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAT602_tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccess aDataAccess = new DataAccess();
+            Console.WriteLine("Message is " +aDataAccess.AddUserName("Zoic"));
+            Console.ReadLine();
+        }
+    }
+}
