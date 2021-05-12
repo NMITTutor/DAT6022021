95e3bf3ed0766f56dae3a3ad0926e8c9f5789102
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2021-05-12 15:15:31 +1200

    Create Player.cs
    Used to receive Player records when getting all players - see DataAccess class

---
+++ Player.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAT201_tester
+{
+    class Player
+    {
+        public static Player CurrentPlayer { get; set; }
+        public string UserName;
+        public int Strength;
+        public int X;
+        public int Y;
+    }
+}
