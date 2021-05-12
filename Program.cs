c16c42637362f2d250578343d90536ed05a46d1e
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2021-05-12 15:09:45 +1200

    Update Program.cs
    Example using DataAccess class and retrieving Many records

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +11,25 @@ namespace DAT602_tester
         static void Main(string[] args)
         {
             DataAccess aDataAccess = new DataAccess();
-            Console.WriteLine("Message is " +aDataAccess.AddUserName("Zoic"));
+
+            // Testing addUserName --existing user name
+            Console.WriteLine("Testing AddUserName");
+            Console.WriteLine("===============================================");
+
+            Console.WriteLine("Message is " + aDataAccess.AddUserName("Zoic"));
+            Console.WriteLine("----------------------------------------------");
+            // Testing Get All Players
+            Console.WriteLine("Testing Get All Players");
+            Console.WriteLine("===============================================");
+
+            foreach ( var p in aDataAccess.GetAllPlayers())
+            {
+                Console.WriteLine(p.UserName + " " + p.Strength);
+            }
+            Console.WriteLine("----------------------------------------------");
+
             Console.ReadLine();
         }
     }
+
 }
